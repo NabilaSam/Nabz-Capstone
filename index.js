4e82ad470fe66e047e9afe1a7466e926e62f59ef
Nabila <nabzluciano@gmail.com>
2023-03-25 23:38:22 +0200

    Tough out here

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ const { errorHandling } = require('./middleware/ErrorHandling');
 const cookieParser = require('cookie-parser');
 
 
+app.use(route);
+app.use(
+    cors(),
+    cookieParser(),
+    express.json(),
+    express.urlencoded({ extended: false }).
+    bodyParser.json()
+)
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin')
+    res.header('Access-Control-Allow-Origin', 'https://nintai-no-ie.web.app/')
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "*")
     res.header("Access-Control-Allow-Headers", "*")
     next();
 });
-app.use(route);
-app.use(
-    cors(),
-    cookieParser(),
-    express.json,
-    express.urlencoded({ extended: false })
-)
 
 app.listen(port, () => {
     console.log(`Server is running ${port}`)
