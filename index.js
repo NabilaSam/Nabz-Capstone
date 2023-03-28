bff677734fe3570333635956d4273f8b2a777c72
NabilaSam <114492999+NabilaSam@users.noreply.github.com>
2023-03-28 09:44:35 +0200

    Removing /

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://nintai-no-ie.web.app/")
+    res.header("Access-Control-Allow-Origin", "https://nintai-no-ie.web.app")
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "*")
     res.header("Access-Control-Allow-Headers", "*")
