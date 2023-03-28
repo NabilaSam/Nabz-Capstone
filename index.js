75c2012036002ced02e63113dd1be1cafb93efb3
Nabila <nabzluciano@gmail.com>
2023-03-28 06:05:40 +0200

    restructuring

--- index.js
+++ index.js
@@ -7,29 +7,20 @@ const port = parseInt(process.env.PORT) || 4000;
 const { errorHandling } = require('./middleware/ErrorHandling');
 const route = require('./controller');
 
-
-
-
-
-
-
-app.use(route);
-app.use(
-    (cors()),
-    cookieParser(),
-    bodyParser.json(),
-    express.json(),
-    
-    express.urlencoded({ extended: false })   
-)
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("https://nintai-no-ie.web.app/")
+    res.header("Access-Control-Allow-Origin", "https://nintai-no-ie.web.app/")
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "*")
     res.header("Access-Control-Allow-Headers", "*")
     next();
 });
+app.use(route);
 
 app.listen(port, () => {
     console.log(`Server is running ${port}`)
